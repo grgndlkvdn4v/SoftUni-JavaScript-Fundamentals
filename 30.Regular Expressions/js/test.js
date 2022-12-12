3c785f97ab2a7249c15e3809b8e13244f8c26c90
Georgi Nedyalkov <grgndlkvdn4v@gmail.com>
2022-12-12 17:11:53 +0200

    Update test.js

--- 30.Regular Expressions/js/test.js
+++ 30.Regular Expressions/js/test.js
@@ -1,19 +1,22 @@
 function test() {
   
-  dummyText = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took 1587s a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic  typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.';
+  let a = 10
+  let b = 20;
+  let c = a > b ? a : b;
+  console.log(c);
 
-  let pattern = /(\d+)(s)/g;
-
-  let matches = dummyText.match(pattern);
-  let execMatches = pattern.exec(dummyText);
-  // console.log(matches);
-
-  while (execMatches !== null) {
-    console.log( execMatches );
-    execMatches = pattern.exec(dummyText);
-  }
-
-
+  // for (let i = 10; i > 3; i -= 2) {
+  //   console.log(i);
+  // }
 
 }
+
 test();
+
+
+function printText(text) {
+    console.log("I love" + text);
+}
+printText('JavaScript');
+
+console.log(Number(true));
